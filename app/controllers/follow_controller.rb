516cb722eb4675f569a2de7af344c4f3481f8bcf
sato_hid <satohide3100@gmail.com>
2018-12-31 08:33:21 +0900

    heroku

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -5,15 +5,30 @@ class FollowController < ApplicationController
     @twitterActive = Account.find_by(user_id:current_user.id,sns_type:1,active_flg:1)
     if @twitterActive
       @twitterActiveImage = Analyze.find_by(account_id:@twitterActive.id)
+      @follows1 = Follow.where(account_id:@twitterActive.id)
+    else
+      @follows1 = []
     end
 
     @instagramActive = Account.find_by(user_id:current_user.id,sns_type:2,active_flg:1)
     if @instagramActive
       @instagramActiveImage = Analyze.find_by(account_id:@instagramActive.id)
+      @follows2 = Follow.where(account_id:@instagramActive.id)
+    else
+      @follows2 = []
     end
 
-    @follows1 = Follow.where(account_id:@twitterActive.id)
-    @follows2 = Follow.where(account_id:@instagramActive.id)
+    @facebookActive = Account.find_by(user_id:current_user.id,sns_type:3,active_flg:1)
+    if @facebookActive
+      @facebookActiveImage = Analyze.find_by(account_id:@facebookActive.id)
+      @follows3 = Follow.where(account_id:@facebookActive.id)
+    else
+      @follows3 = []
+    end
+
+
+
+
   end
   def create
 
