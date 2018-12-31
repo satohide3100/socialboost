0b5157671bd6aea5eda0af4d31cd05d1a263fab5
sato_hid <satohide3100@gmail.com>
2018-12-31 16:57:19 +0900

    heroku

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,7 +1,13 @@
 class PostController < ApplicationController
   before_action :authenticate_user!
   def bulk
+    @twitterActive = Account.find_by(user_id:current_user.id,sns_type:1,active_flg:1)
+    @instagramActive = Account.find_by(user_id:current_user.id,sns_type:2,active_flg:1)
+    @facebookActive = Account.find_by(user_id:current_user.id,sns_type:3,active_flg:1)
   end
   def auto
+    @twitterActive = Account.find_by(user_id:current_user.id,sns_type:1,active_flg:1)
+    @instagramActive = Account.find_by(user_id:current_user.id,sns_type:2,active_flg:1)
+    @facebookActive = Account.find_by(user_id:current_user.id,sns_type:3,active_flg:1)
   end
 end
--- app/controllers/unfollow_controller.rb
+++ app/controllers/unfollow_controller.rb
@@ -1,9 +1,28 @@
 class UnfollowController < ApplicationController
   before_action :authenticate_user!
   def list
-    @twitterActive = Account.where(user_id:current_user.id).where(sns_type:1).where(active_flg:1)
-    @twitterActiveImage = Analyze.where(account_id:@twitterActive[0].id)
-    @instagramActive = Account.where(user_id:current_user.id).where(sns_type:2).where(active_flg:1)
-    @instagramActiveImage = Analyze.where(account_id:@instagramActive[0].id)
+    @twitterActive = Account.find_by(user_id:current_user.id,sns_type:1,active_flg:1)
+    if @twitterActive
+      @twitterActiveImage = Analyze.find_by(account_id:@twitterActive.id)
+      #@follows1 = Follow.where(account_id:@twitterActive.id)
+    else
+      #@follows1 = []
+    end
+
+    @instagramActive = Account.find_by(user_id:current_user.id,sns_type:2,active_flg:1)
+    if @instagramActive
+      @instagramActiveImage = Analyze.find_by(account_id:@instagramActive.id)
+      #@follows2 = Follow.where(account_id:@instagramActive.id)
+    else
+      #@follows2 = []
+    end
+
+    @facebookActive = Account.find_by(user_id:current_user.id,sns_type:3,active_flg:1)
+    if @facebookActive
+      @facebookActiveImage = Analyze.find_by(account_id:@facebookActive.id)
+      #@follows3 = Follow.where(account_id:@facebookActive.id)
+    else
+      #@follows3 = []
+    end
   end
 end
