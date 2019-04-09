c95ea55de2f9479313cc965a13b5cfbc3cc196b5
sato_hid <satohide3100@gmail.com>
2019-04-09 15:19:10 +0900

    heroku

--- app/jobs/add_fav_job.rb
+++ app/jobs/add_fav_job.rb
@@ -430,6 +430,7 @@ class AddFavJob < ApplicationJob
           if favCount < count
             while target_usernameList.count != favCount
               sleep(5)
+              puts driver.find_element(tag_name: "body").text
               gridCount = driver.find_elements(xpath: '/html/body/div[3]/div/div[2]/div/div/div').count
               driver.find_element(xpath: "/html/body/div[3]/div/div[2]/div/div/div#{[gridCount - 1]}").location_once_scrolled_into_view
               driver.find_elements(xpath: '/html/body/div[3]/div/div[2]/div/div/div/div[2]/div/div/a/div/div/div').each do |e|
@@ -439,6 +440,7 @@ class AddFavJob < ApplicationJob
           else
             while target_usernameList.count < count
               sleep(5)
+              puts driver.find_element(tag_name: "body").text
               gridCount = driver.find_elements(xpath: '/html/body/div[3]/div/div[2]/div/div/div').count
               driver.find_element(xpath: "/html/body/div[3]/div/div[2]/div/div/div#{[gridCount - 1]}").location_once_scrolled_into_view
               driver.find_elements(xpath: '/html/body/div[3]/div/div[2]/div/div/div/div[2]/div/div/a/div/div/div').each do |e|
