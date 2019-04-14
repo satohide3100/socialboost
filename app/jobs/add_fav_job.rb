682dbecbf842e04b49d91db172f9cd7258bf70dd
sato_hid <satohide3100@gmail.com>
2019-04-14 20:30:16 +0900

    heroku

--- app/jobs/add_fav_job.rb
+++ app/jobs/add_fav_job.rb
@@ -430,7 +430,7 @@ class AddFavJob < ApplicationJob
           if favCount < count
             while target_usernameList.count != favCount
               sleep(1)
-              gridCount = driver.find_elements(xpath: '/html/body/div[3]/div/div[2]/div/div/div').count
+              gridCount = driver.find_elements(xpath: '/html/body/div[4]/div/div[2]/div/div/div').count
               driver.find_element(xpath: "/html/body/div[3]/div/div[2]/div/div/div#{[gridCount - 1]}").location_once_scrolled_into_view
               driver.find_elements(xpath: '/html/body/div[3]/div/div[2]/div/div/div/div[2]/div/div/a/div/div/div').each do |e|
                 target_usernameList << e.text
@@ -439,7 +439,7 @@ class AddFavJob < ApplicationJob
           else
             while target_usernameList.count < count
               sleep(1)
-              gridCount = driver.find_elements(xpath: '/html/body/div[3]/div/div[2]/div/div/div').count
+              gridCount = driver.find_elements(xpath: '/html/body/div[4]/div/div[2]/div/div/div').count
               driver.find_element(xpath: "/html/body/div[3]/div/div[2]/div/div/div#{[gridCount - 1]}").location_once_scrolled_into_view
               driver.find_elements(xpath: '/html/body/div[3]/div/div[2]/div/div/div/div[2]/div/div/a/div/div/div').each do |e|
                 target_usernameList << e.text
