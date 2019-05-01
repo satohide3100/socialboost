10f31e3be154be67d98130397a0abbbe77a251aa
sato_hid <satohide3100@gmail.com>
2019-05-01 15:29:10 +0900

    heroku

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -11,7 +11,7 @@ class NotificationController < ApplicationController
       @notifications = Notification.order("created_at desc").where(user_id:current_user.id).where(account_id:params[:id]).where("notification_type = 10 or notification_type = 11")
       @target_account = Account.find(@notifications.first.account_id).username
     else
-      @notifications = Notification.order("created_at desc").where(user_id:current_user.id).where("notification_type = 10 or notification_type = 11")
+      @notifications = Notification.order("created_at desc").where(user_id:current_user.id).where("notification_type = 10 or notification_type = 11").where.not(account_id:nil)
       @target_account = "全ユーザー"
     end
   end
